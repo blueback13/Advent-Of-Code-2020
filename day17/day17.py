5e4f1e8d97d4e5a380d213657894a844e8e9c859
Ollie Jackson <Ollie@thejackos.net>
2026-06-16 05:52:15 +0930

    Day 17 part 1: current off by one on the test input

--- day17/day17.py
+++ day17/day17.py
@@ -14,6 +14,15 @@ log = logging.getLogger(__name__)
 
 Point = collections.namedtuple("Point", ["x", "y", "z"])
 
+# Calculate relative positions of points that surround a point on a 3D grid
+relative_surrounding = {
+    (x, y, z)
+    for x in range(-1,2)
+    for y in range(-1,2)
+    for z in range(-1,2)
+    if not x == y == z == 0
+}
+
 
 ################################################################################
 
@@ -22,6 +31,19 @@ class State(enum.Enum):
     """Represents the state of a point in the 3D grid"""
     active = "#"
     inactive = "."
+
+
+################################################################################
+
+
+def get_surrounding_points(point: Point) -> set[Point]:
+    """Return all points that surround a given point"""
+    for x, y, z in relative_surrounding:
+        yield Point(
+            x = point.x + x,
+            y = point.y + y,
+            z = point.z + z,
+        )
 
 
 ################################################################################
@@ -68,6 +90,10 @@ class Grid():
         for point in self._active:
             yield point
 
+    def __len__(self) -> int:
+        """Return number of active points"""
+        return len(self._active)
+
     def __repr__(self) -> str:
         return f"Grid(active={self._active})"
 
@@ -98,6 +124,36 @@ class Grid():
         else:
             self.set_inactive(point)
 
+    def part1_run_cycle(self) -> None:
+        """
+        Update self as per the rules from part 1
+
+        Rules:
+        - If a cube (point) is active and exactly 2 or 3 of its neighbors are
+          also active, the cube remains active. Otherwise, the cube becomes
+          inactive.
+        - If a cube (point) is inactive but exactly 3 of its neighbors are
+          active, the cube becomes active. Otherwise, the cube remains inactive.
+        """
+        # Dict; each point that has a key in the dict has at least one active
+        # point as a neighbour
+        # Type hint: dict[Point, set[Point]]
+        active_adjacent = {}
+
+        for active in self:
+            for neighbour in get_surrounding_points(active):
+                current_neighbouring = active_adjacent.get(neighbour, set())
+                current_neighbouring.add(active)
+                active_adjacent[neighbour] = current_neighbouring
+
+        for point, active_neighbours in active_adjacent.items():
+            # Note: any given point will not have its state changed until we
+            # check it here, so we can rely on the is_active() method here.
+            if self.is_active(point) and not (2 <= len(active_neighbours) <= 3):
+                self.set_inactive(point)
+            elif not self.is_active(point) and len(active_neighbours) == 3:
+                self.set_active(point)
+
 
 ################################################################################
 
@@ -116,12 +172,13 @@ if __name__ == "__main__":
 
     opts = parser.parse_args()
 
-    #data = read_input(opts.input_file)
-
-    print_input(rules, your_ticket, nearby_tickets)
+    grid = Grid.from_file(opts.input_file)
 
     if opts.part1:
-        result1 = "TODO"
+        for i in range(0, 6):
+            grid.part1_run_cycle()
+            log.info("Iteration %s; %s cubes active", i, len(grid))
+        result1 = len(grid)
         print(f"Part1: {result1}")
 
     if opts.part2:
